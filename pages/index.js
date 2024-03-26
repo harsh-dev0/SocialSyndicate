5471dfd169ccad83f2b4996674d83611b6b4d822
harshpa324 <harshpa324@gmail.com>
2024-03-26 23:03:32 +0530

    done

--- pages/index.js
+++ pages/index.js
@@ -5,7 +5,11 @@ export default function Home() {
   return (
     <>
       <Head>
-        <title>Social Syndicate Desgin</title>
+        <title>Social Syndicate</title>
+        <meta name="description" content="Social Syndicate - Elevate Your Business: Unleash the Power of Expert Editing and Strategic Marketing
+ " >
+      
+      </meta>
       </Head>
       <Hero />
     </>
